5ab3355182cfba63c6f69849fde5ba7a43491107
Kayla <sullenspartyof3@gmail.com>
2023-09-07 10:02:13 -0700

    Started index.js and installed inquirer

---
+++ index.js
@@ -0,0 +1,42 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Square, Circle } = require("./lib/shapes");
+
+
+// Array of questions for user input
+const init = () => {
+    inquirer.prompt([{
+    type: 'input',
+    name: 'characters',
+    message: 'Please enter up to 3 characters for your logo.',
+},
+{
+    type: 'input',
+    name: 'text-color',
+    message: 'Please enter a text color.',
+},
+{
+    type: 'list',
+    name: 'shape-type',
+    message: 'Please select a shape for your logo:',
+    choices: [
+        'Triangle', 
+        'Square', 
+        'Circle', 
+    ],
+},
+{
+    type: 'input',
+    name: 'shape-color',
+    message: 'Please enter a shape color.',
+},
+])}.then((answers) => {
+    const svg = generateLogo(answers);
+
+    const fileName = './examples/logo.svg';
+    fs.writeFile(fileName, svg, (err) => {
+        err ? console.log(err) : console.log(`Generated logo.svg`);
+    })
+});
+
+init();
