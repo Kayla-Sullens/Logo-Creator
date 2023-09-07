b7f780c94f3c4119151a623a8a011b76cef4fa24
Kayla <sullenspartyof3@gmail.com>
2023-09-07 11:23:45 -0700

    Added generateLogo const

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Triangle, Square, Circle } = require("./lib/shapes.js");
-
+const { Triangle, Square, Circle } = require('./lib/shapes.js');
+const generateLogo = require('./examples')
 
 // Array of questions for user input
 const init = () => {
